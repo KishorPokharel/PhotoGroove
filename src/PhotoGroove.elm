4da3be199870b0b777ab9fc84632803b3551dc8a
KishorPokharel <kishorpokharel7@gmail.com>
2023-02-27 22:01:15 +0545

    adding optional type annotations; refactor if-expression to cas-expression

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,42 +1,77 @@
 module PhotoGroove exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
-urlPrefix = 
+
+urlPrefix : String
+urlPrefix =
     "https://elm-in-action.com/"
 
+
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo, selectedUrl : String }
+
+
+initialModel : Model
 initialModel =
     { photos =
-        [ { url = "1.jpeg"}
-        , { url = "2.jpeg"}
-        , { url = "3.jpeg"}
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
     }
 
-update msg model =
-    if msg.description == "ClickedPhoto" then
-        { model | selectedUrl = msg.data }
-    else
-        model
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg.description of
+        "ClickedPhoto" ->
+            { model | selectedUrl = msg.data }
+
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "2.jpeg" }
+
+        _ ->
+            model
+
+
+type alias Msg =
+    { description : String, data : String }
+
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
-        [ h1 [] [text "Photo Groove" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , button [ onClick { description = "ClickedSurpriseMe", data = "" } ] [ text "Surprise Me" ]
         , div [ id "thumbnails" ] (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ] []
         ]
 
+
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
-    img 
-        [ classList [("selected", selectedUrl == thumb.url)]
+    img
+        [ classList [ ( "selected", selectedUrl == thumb.url ) ]
         , src (urlPrefix ++ thumb.url)
         , onClick { description = "ClickedPhoto", data = thumb.url }
         ]
         []
 
+
 main =
-    Browser.sandbox { init=initialModel, view=view, update=update }
+    Browser.sandbox { init = initialModel, view = view, update = update }
